a3e2a88d9836649aaed32d1f3439beedc8bc174a
Azamat <azamalik.011124@gmail.com>
2024-07-29 13:17:56 +0300

    save

--- script.js
+++ script.js
@@ -1,118 +1,70 @@
 
 
-let counter =0;
-while(counter<=100){
-    console.log(counter);
-    counter =counter +1
-}
+// ! задача -1
 
-zadacha-2
+const student = {
+  name: "Azamat",
+  lastName: "Malikov",
+  age: 22,
+};
+const student2 = Object.assign({}, student);
+delete student2.age;
+console.log(student);
+console.log(student2);
 
-let count =0;
-while (count <=20){
-    if (count % 2===0){
-        console.log(count);
+// ! задача - 3
+
+const array = [
+  1,
+  true,
+  3,
+  { name: "ZH", hobbies: ["swimming", "reading"] },
+  5,
+  "hello",
+  "hi",
+];
+console.log(array[1]);
+
+console.log(array[4]);
+
+console.log(array[3].hobbies);
+
+array[3].hobbies.pop();
+console.log(array[3].hobbies);
+
+array.unshift("jyldyz krasavchik");
+array.push("merhaba");
+array.shift();
+console.log(array);
+
+const res = array.findIndex((element) => element === "hello");
+console.log(res);
+
+const ress = array.slice(0, 3);
+console.log(ress);
+
+array[3].hobbies.unshift("football");
+console.log(array[3].hobbies);
+
+delete array[4];
+delete array[5];
+console.log(array);
+
+console.log(array.length);
+
+//! задача - 4
+
+const aray = [1, 2, 3, "sdf", 4, "sdf", {}, 3, true, 2, [1, 6, 3], 1];
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+  if (typeof array[i] === "number") {
+    sum = sum + array[i];
+  } else if (typeof array[i] === "object") {
+    for (let k = 0; k < array[i].length; k++) {
+      if (typeof array[i][k] == "number") {
+        sum = sum + array[i][k];
+      }
     }
-    count++;
-
-}
-
-
-
-let counter = 1;
-let sum = 1;
-
-while (counter <= 0 ) {
-  sum = sum + counter;
-  counter++;
-}
-console.log(sum +counter);
-
-
-zadacha-4
-
-let userName ="Naruto Shipuden  ";
-
-console.log(userName.charAt(0));
-let counter =0;
-while (counter <= userName.length){
-    if(userName.charAt(counter) ==="u"){
-        console.log("the Leter is in "+counter+"index");
-    }
-    counter ++;
-}
-let naruto ="Naruto Shipuden";
-for(let index =naruto.length; index>=0; index--){
-    let char =naruto.charAt(index);
-    if (char ==="u"){
-        console.log(yes! ${char});
-    }else{
-        console.log("No!");
-    }
-}
-
-
-
-
-let user = "peaksoft";
-let coun = user.length;
-
-while (coun >= 0) {
-    console.log(user.charAt(coun));
-    coun--;
-}
-
-segodna
-let number = prompt("введите код");
-for (let kod = 0; kod <= number; kod++){
-    console.log(kod);
-}
-
-
-
-let string = 0;
-let st = ``;
-
-while (string < 6) {
-  let strin = 0;
-  while (strin <= 10) {
-    if (string === 0 || string === 5) {
-      st = st + "*";
-    }  else if (strin === 0 || strin === 10){
-        st = st + "*"
-    }
-
-    else {
-      st = st + "-";
-    }
-
-    strin++;
   }
-
-  st = st + "\n";
-  string++;
 }
-
-console.log(st);
-
- 
-
-let strogo = 7;
-let strogo2 = "";
-let strogo3 = "#"
-
-for (let ts = 0; ts < strogo; ts++){
-    strogo2 += strogo3;
-    console.log(strogo2);
-}
-
-let wl = 7;
-let res1 = "";
-let res2 = "#";
-let res3 = 0;
-
-while (wl >= res3) {
-  res1 += res2;
-  console.log(res1);
-  res3++;
-}
+console.log(sum);
