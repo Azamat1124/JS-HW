0e2a319e87355a6c33baff745c8b78b76d2a3a12
Azamat <azamalik.011124@gmail.com>
2024-08-17 14:41:47 +0300

    save

--- script.js
+++ script.js
@@ -1,30 +1,77 @@
 
-const form2 = document.getElementById("form");
-form2.addEventListener("submit", (event) => {
-  event.preventDefault();
 
-  const input2 = document.getElementById("input").value;
-  if(input2.length===0){
-    const divclass = document.getElementById("divclass")
-    divclass.style.display = "block";
-    const button5 = document.getElementById("button5")
-    button5.addEventListener("click",()=>{
-        divclass.style.display = "none"
-    })
-}else{
-  const ul = document.getElementById("ulid");
 
-  const liclass = document.createElement("li");
-  liclass.className = "li";
 
-  const button2 = document.createElement("button");
-  button2.className = "button22";
-  button2.textContent = "delete";
-  liclass.append(input2, button2);
-  button2.addEventListener("click", () => {
-    button2.parentElement.remove();
-  });
-  ul.appendChild(liclass);
+// ! задача -1
+const taimer = document.getElementById("taim")
+setTimeout(()=>{
+    taimer.textContent = "ураа";
+console.log(setTimeout);
+
+},10000)
+
+// ! задача -2
+
+const divgreen = document.getElementById("divgreen");
+
+let backgroundColor = ["green", "blue", "aqua", "cadetblue", "blueviolet", "crimson"];
+
+let shot = 0;
+function inter() {
+  divgreen.style.backgroundColor = backgroundColor[shot];
+  shot++;
+  if (shot > backgroundColor.length - 1) {
+  shot = 0;
+
 }
+}
+setInterval(inter, 1000);
 
-});
+
+// ! задача -3
+
+const minute =document.getElementById("minute");
+const seconds =document.getElementById("seconds");
+
+const divshot = document.getElementById("div-shot");
+const buttonstart = document.getElementById("button-start");
+const buttonstop = document.getElementById("button-stop");
+const buttonreset = document.getElementById("button-reset");
+
+let interval = null
+
+buttonstart.addEventListener("click",()=>{ 
+    
+    let time = parseInt(minute.value) *  60 + parseInt(seconds.value);
+    interval = setInterval(()=>{
+        let minute_2 = Math.round(time/60)
+        let seconds_2 = time % 60
+        divshot.innerHTML = `
+        ${minute_2}
+        :
+        ${seconds_2 < 10 ? "0"+seconds_2 : seconds_2}
+        `;
+        time--;
+        if(time < 0){
+            clearInterval(interval)
+        }
+    },1000)
+    
+})
+buttonstop.addEventListener("click",()=>{
+    clearInterval(interval)
+})
+buttonreset.addEventListener("click",()=>{
+    clearInterval(interval)
+    divshot.innerHTML = "00:00"
+})
+minute.innerHTML = "";
+seconds.innerHTML = "";
+
+
+
+
+
+
+
+
